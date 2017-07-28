fc557767998e7da06549a62b38e27d59fc49ba05
Mark Pinero <markspinero@gmail.com>
2017-07-28 00:37:52 -0700

    wip: sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,12 @@
 const sidebarItems = document.querySelectorAll('.navigation__item');
+const sidebarMenus = [...sidebarItems].filter(item => item.contains(item.querySelector('.navigation__list')));
 
-console.log([...sidebarItems].filter(item => {
-  return item.contains(item.querySelector('.navigation__list'))
-}));
+const toggleMenu = (event) => {
+  event.preventDefault();
+  console.log('test');
+};
+
+sidebarMenus.forEach((menu) => {
+  const caret = menu.querySelector('svg');
+  caret.addEventListener('click', toggleMenu);
+});
