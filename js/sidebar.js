918a34e832060784e30d02ab763bcf154bf8b392
Mark Pinero <markspinero@gmail.com>
2017-07-31 12:37:20 -0700

    add dropdown functionality

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,3 +1,16 @@
+const sidebarDropdown = document.querySelector('.sidebar__dropdown');
+const sidebarVersions = document.querySelector('.sidebar__dropdown-list');
+
+function toggleDropdown() {
+  if (!sidebarDropdown.classList.contains('sidebar__dropdown--active')) {
+    sidebarDropdown.classList.add('sidebar__dropdown--active');
+  } else {
+    sidebarDropdown.classList.remove('sidebar__dropdown--active');
+  }
+}
+
+sidebarDropdown.addEventListener('click', toggleDropdown);
+
 const sidebarItems = document.querySelectorAll('.navigation__item');
 const sidebarMenus = [...sidebarItems].filter(item => item.contains(item.querySelector('.navigation__list')));
 
@@ -11,7 +24,7 @@ function toggleMenu(event) {
   }
 }
 
-sidebarMenus.forEach((menu) => {
+sidebarMenus.forEach(menu => {
   const caret = menu.querySelector('svg');
   caret.addEventListener('click', toggleMenu);
 });
