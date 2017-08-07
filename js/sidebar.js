9eadf1f237e8fea09e99f01db479aad926c104ea
Blake Wood <blakeandrewwood@gmail.com>
2017-08-07 12:52:18 -0700

    clean up js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,19 @@
 const sidebarDropdown = document.querySelector('.sidebar__dropdown');
+const sidebarItems = document.querySelectorAll('.navigation__item');
+const sidebarMenus = [...sidebarItems].filter(item => item.contains(item.querySelector('.navigation__list')));
 
+document.addEventListener('click', detectClick);
+sidebarDropdown.addEventListener('click', toggleDropdown);
+
+sidebarMenus.forEach((menu) => {
+  const caret = menu.querySelector('svg');
+  caret.addEventListener('click', toggleMenu);
+});
+
+/**
+ *
+ *
+ */
 function toggleDropdown() {
   if (!sidebarDropdown.classList.contains('sidebar__dropdown--active')) {
     sidebarDropdown.classList.add('sidebar__dropdown--active');
@@ -8,18 +22,20 @@ function toggleDropdown() {
   }
 }
 
+/**
+ *
+ *
+ */
 function detectClick(event) {
   if (sidebarDropdown.classList.contains('sidebar__dropdown--active') && !sidebarDropdown.contains(event.target)) {
     sidebarDropdown.classList.remove('sidebar__dropdown--active');
   }
 }
 
-document.addEventListener('click', detectClick);
-sidebarDropdown.addEventListener('click', toggleDropdown);
-
-const sidebarItems = document.querySelectorAll('.navigation__item');
-const sidebarMenus = [...sidebarItems].filter(item => item.contains(item.querySelector('.navigation__list')));
-
+/**
+ *
+ *
+ */
 function toggleMenu(event) {
   event.preventDefault();
   const subHead = this.parentNode.parentNode;
@@ -30,7 +46,3 @@ function toggleMenu(event) {
   }
 }
 
-sidebarMenus.forEach((menu) => {
-  const caret = menu.querySelector('svg');
-  caret.addEventListener('click', toggleMenu);
-});
