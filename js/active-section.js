afebcda79ab243dbf4c7efb3068d9fe788f81a4e
Mark Pinero <markspinero@gmail.com>
2017-08-08 15:01:38 -0700

    add sticky container, tweak styling

--- js/active-section.js
+++ js/active-section.js
@@ -1,7 +1,20 @@
-const contentArea = document.querySelector('.content');
+const contentHeadings = document.querySelectorAll('.content h2');
 
-const contentHeadings = contentArea.querySelectorAll('h2');
+if (contentHeadings && window.innerWidth > 1025) {
+  // create div or part of template?
+  const activeElement = document.querySelector('.content__sections-sticky');
+  const stickyStart = activeElement.getBoundingClientRect().top;
 
-if (contentHeadings) {
-  console.log(contentHeadings);
+  const stickyState = () => {
+    const scroll = window.scrollY;
+    if (scroll > 0) {
+      activeElement.classList.add('content__sections-sticky--fixed');
+      activeElement.style[top] = stickyStart;
+    } else {
+      activeElement.classList.remove('content__sections-sticky--fixed');
+      activeElement.style[top] = null;
+    }
+  };
+
+  document.addEventListener('scroll', stickyState);
 }
