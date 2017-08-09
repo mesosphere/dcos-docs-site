33e055dddd833088508f06e5de481651c3736d67
Mark Pinero <markspinero@gmail.com>
2017-08-08 19:13:54 -0700

    wip: sticky toc, add mobile-only media query

--- js/active-section.js
+++ js/active-section.js
@@ -3,16 +3,22 @@ const contentHeadings = document.querySelectorAll('.content h2');
 if (contentHeadings && window.innerWidth > 1025) {
   // create div or part of template?
   const activeElement = document.querySelector('.content__sections-sticky');
-  const stickyStart = activeElement.getBoundingClientRect().top;
+  const bodyOffset = document.body.getBoundingClientRect().top;
+  const firstHeading = contentHeadings[0].getBoundingClientRect().top;
+  const offset = firstHeading - bodyOffset;
+  console.log(offset);
+  // const firstHeadingOffset = window.clientTop;
+  activeElement.classList.add('content__sections-sticky--fixed');
+  activeElement.style['top'] = `${offset}px`;
+  // const stickyStart = activeElement.getBoundingClientRect().top;
 
   const stickyState = () => {
-    const scroll = window.scrollY;
-    if (scroll > 0) {
-      activeElement.classList.add('content__sections-sticky--fixed');
-      activeElement.style[top] = stickyStart;
+    const pageScroll = window.scrollY;
+    const offsetY = offset - pageScroll;
+    if (pageScroll > offsetY + 100) {
+      activeElement.style['top'] = '100px';
     } else {
-      activeElement.classList.remove('content__sections-sticky--fixed');
-      activeElement.style[top] = null;
+      activeElement.style['top'] = `${offsetY}px`;
     }
   };
 
