ea0397b96f737f351661e5b0ce60ffb84b139b01
Mark Pinero <markspinero@gmail.com>
2017-09-11 01:00:59 -0700

    implement fixed position toc

--- js/sticky-toc.js
+++ js/sticky-toc.js
@@ -1,14 +1,16 @@
+const contentContainer = document.querySelector('.content__container');
 const contentHeadings = document.querySelectorAll('.content__container [id]');
-const contentHeadingsArray = [...contentHeadings];
+const contentHeadingsArray = [...contentHeadings].filter((el) => !el.dataset.hide);
 
 if (contentHeadings && window.innerWidth > 1025) {
   const tableOfContents = document.querySelector('.content__sections');
   document.addEventListener('scroll', () => {
     const headerOffset = 120;
     const windowOffset = window.pageYOffset + headerOffset;
-    console.log(tableOfContents.classList.contains('content__sections--top'));
-    if (headerOffset > tableOfContents.getBoundingClientRect().top) {
-      tableOfContents.classList.add('content__sections--top'); 
+    if (headerOffset > contentContainer.getBoundingClientRect().top) {
+      if(!tableOfContents.classList.contains('content__sections--top')) {
+        tableOfContents.classList.add('content__sections--top'); 
+      }
     } else {
       tableOfContents.classList.remove('content__sections--top');
     }
@@ -19,9 +21,8 @@ if (contentHeadings && window.innerWidth > 1025) {
       }
     }
     contentHeadingsArray.forEach((el) => {
-      const elTopOffset = (el.offsetTop - (el.offsetHeight / 3)) + 120;
-      const elBottomOffset = (el.offsetTop + el.offsetHeight) + 120;
-      if (windowOffset > elTopOffset && elBottomOffset > windowOffset) {
+      const elTopOffset = el.getBoundingClientRect().top;
+      if (headerOffset > elTopOffset) {
         const linkSelector = `a[href='#${el.getAttribute('id')}']`;
         const activeLink = document.querySelector(linkSelector);
         if (activeLink) {
