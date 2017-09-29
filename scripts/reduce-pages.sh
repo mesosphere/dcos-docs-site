2e925fa0ccc3e16f2004261af82629fc84060a8e
Blake Wood <blakeandrewwood@gmail.com>
2017-09-29 14:17:51 -0700

    script fix

--- scripts/reduce-pages.sh
+++ scripts/reduce-pages.sh
@@ -5,8 +5,9 @@
 # Description: Removes majority of content pages for quicker development builds
 #
 
-rm -rf "./pages/docs/1.10/!(index.md|overview|img)"
-rm -rf "./pages/docs/1.9/!(index.md)"
-rm -rf "./pages/docs/1.8/!(index.md)"
-rm -rf "./pages/docs/1.7/!(index.md)"
-rm -rf "./pages/service-docs/!(index.md|overview|img)"
+shopt -s extglob
+rm -rf ./pages/docs/1.10/!(index.md|overview|img)
+rm -rf ./pages/docs/1.9/!(index.md)
+rm -rf ./pages/docs/1.8/!(index.md)
+rm -rf ./pages/docs/1.7/!(index.md)
+rm -rf ./pages/service-docs/!(index.md)
