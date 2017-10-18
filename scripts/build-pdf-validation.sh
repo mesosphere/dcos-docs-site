3746f46405ee2410af44e2c1857888abce9b00fc
Blake Wood <blakeandrewwood@gmail.com>
2017-10-18 15:17:33 -0700

    added docker test config

--- scripts/build-pdf-validation.sh
+++ scripts/build-pdf-validation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Usage:       build-validation.sh
+# Usage:       build-pdf-validation.sh
 #
 # Description: Validates there was a docker image build
 #
 
-docker_image_build_id=$(docker images -q mesosphere/dcos-docs:latest)
+docker_image_build_id=$(docker images -q mesosphere/dcos-docs-pdf:latest)
 
 if [[ "$docker_image_build_id" == "" ]]; then
   exit 1
