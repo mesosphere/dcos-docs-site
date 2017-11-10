48b24f6a7cb1cebc5731847bf0430596977c42ac
Karl Isenberg <karl@mesosphere.io>
2017-11-09 17:10:45 -0800

    Add scripts for CI to build, test, publish, and deploy dcos-docs-site.
    - ci/0-commit-hook.sh will be triggered by commits to master, develop, and staging branches
    - CI will provide a bunch of required env variables, listed in ci/0-commit-hook.sh

---
+++ ci/0-commit-hook.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+project_dir=$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)
+cd "${project_dir}"
+
+# required inputs (fail early w/ nounset)
+DOCKER_USER="${DOCKER_USER}"
+DOCKER_PASS="${DOCKER_PASS}"
+APP_ID="${APP_ID}" # ex: /docs-site/docs2-dev
+DCOS_URL="${DCOS_URL}" # ex: https://leader.mesos
+DCOS_USER_NAME="${DCOS_USER_NAME}" # ex: docs-bot
+DCOS_USER_PRIVATE_KEY_PATH="${DCOS_USER_PRIVATE_KEY_PATH}" # ex: docs-bot-private.pem
+DCOS_CRT="${DCOS_CRT}" # ex: docs-us.crt
+
+# optional inputs
+# CONTAINER_NAME # default: dcos-docs-site
+
+ci/1-setup-env.sh
+
+# capture and print to stdout (tty)
+IMAGE="$(ci/2-build-site.sh | tee /dev/tty | grep "^Image: " | sed "s/^Image: //" )"
+export IMAGE_NAME="$(echo "${IMAGE}" | sed 's/^\(.*\):\([^:]*\)/\1/')"
+export IMAGE_TAG="$(echo "${IMAGE}" | sed 's/^\(.*\):\([^:]*\)/\2/')"
+
+ci/3-test-site-image.sh
+
+ci/4-publish-site-image.sh
+
+ci/5-deploy-site-update.sh
---
+++ ci/2-build-site-image.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Build the site and package it in a docker image.
+# Tag the image with the branch, date, and commit short sha.
+
+
+set -o errexit -o nounset -o pipefail
+
+# run from repo root
+project_dir=$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)
+cd "${project_dir}"
+
+GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+echo "Branch: ${GIT_BRANCH}"
+
+echo "Building site and docker image..."
+make docker-build-site
+
+echo "Image Build Success!"
+
+IMAGE_NAME="mesosphere/dcos-docs-site"
+TEMP_IMAGE_TAG="latest"
+
+echo "Tagging docker image..."
+GIT_SHA="$(git rev-parse --short HEAD)"
+DATE="$(date '+%Y-%m-%d')"
+IMAGE_TAG="${GIT_BRANCH}-${DATE}-${GIT_SHA}"
+
+# remove duplicate image if build already happened
+if [[ -n "$(docker image ls -q ${IMAGE_NAME}:${IMAGE_TAG})" ]]; then
+  docker rmi ${IMAGE_NAME}:${IMAGE_TAG}
+fi
+
+docker tag ${IMAGE_NAME}:${TEMP_IMAGE_TAG} ${IMAGE_NAME}:${IMAGE_TAG}
+
+echo "Image Tag Success!"
+echo "Image: ${IMAGE_NAME}:${IMAGE_TAG}"
