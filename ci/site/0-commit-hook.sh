4e5300c3579e5e036168522c10465107af084595
Karl Isenberg <karl@mesosphere.io>
2017-12-01 13:38:12 -0800

    Fix site build script paths

--- ci/site/0-commit-hook.sh
+++ ci/site/0-commit-hook.sh
@@ -18,7 +18,7 @@ DCOS_CRT="${DCOS_CRT}" # ex: docs-us.crt
 # optional inputs
 # CONTAINER_NAME # default: dcos-docs-site
 
-ci/1-setup-env.sh
+ci/site/1-setup-env.sh
 
 # capture output AND print to stdout
 exec 5>&1
@@ -27,14 +27,14 @@ function close_file_descriptor() {
 }
 trap 'close_file_descriptor' EXIT
 
-IMAGE=$(ci/2-build-site-image.sh | tee >(cat - >&5) | grep "^Image: " | sed "s/^Image: //")
+IMAGE=$(ci/site/2-build-site-image.sh | tee >(cat - >&5) | grep "^Image: " | sed "s/^Image: //")
 IMAGE_NAME="$(echo "${IMAGE}" | sed 's/^\(.*\):\([^:]*\)/\1/')"
 export IMAGE_NAME # export separately so errexit works :(
 IMAGE_TAG="$(echo "${IMAGE}" | sed 's/^\(.*\):\([^:]*\)/\2/')"
 export IMAGE_TAG # export separately so errexit works :(
 
-ci/3-test-site-image.sh
+ci/site/3-test-site-image.sh
 
-ci/4-publish-site-image.sh
+ci/site/4-publish-site-image.sh
 
-ci/5-deploy-site-update.sh
+ci/site/5-deploy-site-update.sh
