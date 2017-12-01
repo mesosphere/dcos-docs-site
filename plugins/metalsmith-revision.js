c1857b65023d67c16c957f79cc4dc1a27c828ab2
Blake Wood <blakeandrewwood@gmail.com>
2017-12-01 15:14:14 -0800

    updates

--- plugins/metalsmith-revision.js
+++ plugins/metalsmith-revision.js
@@ -2,30 +2,24 @@ const resolve = require('path').resolve;
 const relative = require('path').relative;
 const fs = require('fs');
 const crypto = require('crypto');
-//const recurse = require('recursive-readdir');
 
-function checksum (str, algorithm, encoding) {
-  return crypto
-    .createHash(algorithm || 'md5')
-    .update(str, 'utf8')
-    .digest(encoding || 'hex')
-}
+var debug = require('debug')('metalsmith-revision');
 
-const default_options = { layout: false, layoutDir: './layouts' }
+const defaultOptions = { layout: false, layoutDir: './layouts' };
 
 function plugin(opts) {
-  const options = Object.assign({}, default_options, opts)
+  const options = Object.assign({}, defaultOptions, opts);
   return (files, metalsmith, done) => {
 
-    const configPath = resolve(metalsmith._directory, '.revision')
-    const hash_table = { layouts: {}, src: {} }
-    const layoutDirectory = resolve(metalsmith._directory, options.layoutDir)
+    const configPath = resolve(metalsmith._directory, '.revision');
+    const hashTable = { layouts: {}, src: {} };
+    const layoutDirectory = resolve(metalsmith._directory, options.layoutDir);
     const revision = fs.existsSync(configPath)
       ? JSON.parse(fs.readFileSync(configPath, 'utf-8'))
       : null;
 
     if(revision == null) {
-      fs.writeFileSync(configPath, JSON.stringify(hash_table), 'utf-8')
+      fs.writeFileSync(configPath, JSON.stringify(hashTable), 'utf-8');
       done();
       return;
     }
@@ -37,27 +31,36 @@ function plugin(opts) {
     metalsmith.revision.cachedFiles = Object.assign({}, files);
 
     Object.keys(files).forEach(file => {
-      const hash = checksum(files[file].contents)
-      hash_table.src[file] = hash
+      const hash = checksum(files[file].contents);
+      hashTable.src[file] = hash;
       if(revision.src[file] === hash) {
         delete files[file];
       }
     });
 
-    console.log('Updating %s files', Object.keys(files).length);
+    debug('%s files updated', Object.keys(files).length);
 
-    fs.writeFileSync(configPath, JSON.stringify(hash_table), 'utf-8')
-    done()
+    fs.writeFileSync(configPath, JSON.stringify(hashTable), 'utf-8');
+    done();
   }
 }
 
 function restore(opts) {
-  const options = Object.assign({}, default_options, opts)
+  const options = Object.assign({}, defaultOptions, opts);
   return (files, metalsmith, done) => {
     files = metalsmith.revision.cachedFiles;
-    console.log('Restored %s files.', Object.keys(files).length);
+
+    debug('%s files restored', Object.keys(files).length);
+
     done();
   }
+}
+
+function checksum (str, algorithm, encoding) {
+  return crypto
+    .createHash(algorithm || 'md5')
+    .update(str, 'utf8')
+    .digest(encoding || 'hex');
 }
 
 module.exports = {
