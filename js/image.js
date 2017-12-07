2d46b20928d82b4d1a475a25d46d039bc56bf407
HypeUFO <michael.maio22@gmail.com>
2017-12-07 14:54:11 -0800

    fix image links

--- js/image.js
+++ js/image.js
@@ -5,7 +5,7 @@ function handleImages() {
       wrapper.setAttribute('style', `max-width:${img.naturalWidth}px;`);
     } else if (img.classList.contains('img--inline')) {
       img.setAttribute('style', `max-width:${img.naturalWidth}px;`);
-    } else {
+    } else if (img.className.length < 1) {
       const a = document.createElement('a');
       a.href = img.src;
       a.setAttribute('target', '_blank');
