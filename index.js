ac6c9ff0644a15aa76cab8aa9a65860be4665619
Blake Wood <blakeandrewwood@gmail.com>
2017-12-14 20:38:15 -0800

    changed site title

--- index.js
+++ index.js
@@ -72,9 +72,10 @@ let currentYear = (new Date()).getFullYear();
 // Metadata
 MS.metadata({
   url: "https://docs.mesosphere.com",
-  siteTitle: "Mesosphere DC/OS",
-  title: "Mesosphere DC/OS",
-  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+  siteTitle: "Mesosphere DC/OS Documentation",
+  siteDescription: "Welcome to the DC/OS documentation. The DC/OS documentation " +
+  "can help you set up, learn about the system, and get your applications and" +
+  " workloads running on DC/OS.",
   copyright: `&copy; ${currentYear} Mesosphere, Inc. All rights reserved.`,
   env: process.env.NODE_ENV,
   gitBranch: process.env.GIT_BRANCH,
