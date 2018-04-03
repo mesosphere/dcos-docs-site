91f7c34bfe1ad9c8a21e5a99ab6d5f2fe5a90427
Vanessa Pitchford Cristobal <rc.vanessa87@gmail.com>
2018-04-03 17:39:26 +0100

    script to compress images

--- scripts/compressimages.js
+++ scripts/compressimages.js
@@ -1,7 +1,8 @@
 #!/bin/node
+
 /* Usage: Script to compress images
-** Description: Images now will be compressed before going into Pandoc
-*/
+ ** Description: Images now will be compressed before going into Pandoc
+ */
 const path = require('path');
 
 const dir = process.argv[2];
@@ -9,17 +10,25 @@ const dir = process.argv[2];
 // expression to identify the files we want to process
 const fileRX = new RegExp(/.*\/(img)/g);
 
-const compress_images = require('compress-images');
+const imagemin = require('imagemin');
 
-// Function to do the compression
-function compressor() {
-  compress_images(`${dir}/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}`, `${dir}/`, { compress_force: false, statistic: true, autoupdate: true }, false,
-    { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
-    { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
-    { svg: { engine: 'svgo', command: '--multipass' } },
-    { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } }, () => {
-      console.log('it compressed', `${dir}/`);
-    });
-}
+const mozJpeg = require('imagemin-mozjpeg');
 
-compressor();
+const gifsicle = require('imagemin-gifsicle');
+
+const svgo = require('imagemin-svgo');
+
+const pngquant = require('imagemin-pngquant');
+
+imagemin(['./pages/**/*.{jpeg,jpg,png,svg,gif}'], './pages/', {
+  plugins: [
+    mozJpeg(),
+    gifsicle(),
+    svgo(),
+    pngquant(),
+  ],
+}).then((files) => {
+  console.log(files);
+}, (error) => {
+  console.log(error);
+});
