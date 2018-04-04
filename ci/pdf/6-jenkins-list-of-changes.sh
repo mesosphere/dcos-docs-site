4dbe56078e82cd05dade1ad9a9c0cca4d63cb99d
Vanessa Pitchford Cristobal <rc.vanessa87@gmail.com>
2018-04-04 18:05:16 +0100

    script for jenkins list of changes.sh

--- ci/pdf/6-jenkins-list-of-changes.sh
+++ ci/pdf/6-jenkins-list-of-changes.sh
@@ -1,32 +1,25 @@
 #!/usr/bin/env groovy
-/*
-** Usage:        Get a list of the changes between builds
-**
-** Description:  Get a list of commits with changes that are in the last successfull commit
-**               in order to split the build of the pdf. This script uses Groovy from Jenkins.
-**
-**
-*/
 
-/* 1. Set the variables so I get the data that was either introduced or changed  */
+#  Usage:        Get a list of the changes between builds
+#
+#  Description:  Get a list of commits with changes that are in the last successfull commit
+#                in order to split the build of the pdf. This script uses Groovy from Jenkins.
+#
 
-/*    *****************
-*     Get information from Last Changes Plugin and set up Env. Variables
-*     *****************
-*/
-pipeline {
-    agent { docker {image 'node:6.3'} }
 
-    stages {
-        stage('build') {
-            steps {
-                sh 'npm --version'
 
-            }
-        }
-    }
-}
 
-/* 2. Find those files and run the scripts to lint */
+    #  *******************************************************************
+    #   Get information from Last Changes Plugin and set up Env.Variables
+    #  *******************************************************************
 
-/* 3. Run the compression script on any images */
+
+def lastSuccessfulCommit = getLastSuccessfulCommit()
+ def currentCommit = commitHashForBuild(currentBuild.rawBuild)
+    if (lastSuccessfulCommit) {
+    commits = sh(
+        script: "git rev-list $currentCommit \"^$lastSuccessfulCommit\"",
+        returnStdout: true
+    ).split('\n')
+    println "Commits are: $commits"
+ }
