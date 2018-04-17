858da40a19e0c621f9da698121537cd85c6cfc04
Vanessa Pitchford Cristobal <rc.vanessa87@gmail.com>
2018-04-17 14:48:08 +0100

    Changes after review

--- scripts/build-pdf.sh
+++ scripts/build-pdf.sh
@@ -13,9 +13,5 @@ if [[ "$docker_image_build_id" != "" ]]; then
   docker rmi $docker_image
 fi
 
-#LOG="THIS IS DEBUG MESSAGE AND ITS VERY IMPORTANT" \
-LOG=$(git diff "${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"..HEAD --name-only | grep \.md)
-echo $LOG
-echo "==========================="
 GIT_BRANCH=$GIT_BRANCH \
 docker-compose -f ./docker/docker-compose.production.yml build --no-cache pdf
