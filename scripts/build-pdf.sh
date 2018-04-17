99a43254754e4e93cd2f66c89eacd6e37da0deb5
Vanessa Pitchford Cristobal <rc.vanessa87@gmail.com>
2018-04-17 16:02:06 +0100

    Cleaning logs

--- scripts/build-pdf.sh
+++ scripts/build-pdf.sh
@@ -13,8 +13,5 @@ if [[ "$docker_image_build_id" != "" ]]; then
   docker rmi $docker_image
 fi
 
-# LATEST_MDFILES="Vanessa is working on this ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-# export LATEST_MDFILES
-
 GIT_BRANCH=$GIT_BRANCH \
 docker-compose -f ./docker/docker-compose.production.yml build --no-cache pdf
