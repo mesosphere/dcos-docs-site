86798765bfbd06795fc582a2c676a0e7ef1345ba
niartenyaw <amwayne90@gmail.com>
2018-07-26 18:39:25 -0700

    Update script to count by section

--- scripts/count_words.rb
+++ scripts/count_words.rb
@@ -1,11 +1,22 @@
 #!/usr/bin/ruby
-file_names = Dir["pages/1.11/**/*.md"]
-puts file_names
+file_names = Dir["pages/services/**/*.md"]
 puts "Number of files: #{file_names.count}"
 
 word_count = 0
 
+curr_package = ""
+curr_version = ""
+
 file_names.each do |filename|
+  next if filename.include?('beta')
+  path = filename.split('/')
+  if path[2] != curr_package || path[3] != curr_version
+    puts "#{curr_package} #{curr_version} #{word_count}"
+    curr_package = path[2]
+    curr_version = path[3]
+    word_count = 0
+  end
+
   file = File.open(filename)
   num_section_breaks = 0
   in_code_break = false
@@ -19,5 +30,3 @@ file_names.each do |filename|
     end
   end
 end
-
-puts "Word count: #{word_count}"
