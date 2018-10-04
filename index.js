df9be520cad76417306a315a9e893caaa4ef9816
niartenyaw <amwayne90@gmail.com>
2018-10-03 18:18:03 -0700

    TEST DO NOT MERGE

--- index.js
+++ index.js
@@ -236,6 +236,8 @@ CB.use(timer('CB: Redirects'));
 CB.use(permalinks());
 CB.use(timer('CB: Permalinks'));
 
+// Hello aaron
+
 // Layouts
 if (!RENDER_PATH_PATTERN) {
   // Default: Render all pages.
