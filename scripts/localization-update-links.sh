f516b843eb2762c0956b2b2467316df57e8a64ed
niartenyaw <amwayne90@gmail.com>
2018-11-30 16:12:06 -0800

    Update localization script

--- scripts/localization-update-links.sh
+++ scripts/localization-update-links.sh
@@ -1,11 +1,22 @@
 # Links and Images
 
-find ./pages/cn -type f -exec sed -i '' -e 's/](\/1.11/](\/cn\/1.11/g' {} \;
-find ./pages/cn -type f -exec sed -i '' -e 's/](1.11/](cn\/1.11/g' {} \;
+## Update 1.11
+#find ./pages/cn -name index.md -type f -exec sed -i '' -e 's/](\/1.11/](\/cn\/1.11/g' {} \;
+#find ./pages/cn -name index.md -type f -exec sed -i '' -e 's/](1.11/](cn\/1.11/g' {} \;
+
+## Update services
+#find ./pages/cn -name index.md -type f -exec sed -i '' -e 's/](\/services/](\/cn\/services/g' {} \;
+#find ./pages/cn -name index.md -type f -exec sed -i '' -e 's/](services/](cn\/services/g' {} \;
+
+# Fix broken links/images
+# find ./pages -name index.md -type f -exec sed -i '' -e 's/] (\//](\//g' {} \;
 
 # Include files
 
-find ./pages/cn -type f -exec sed -i '' -e 's/#include \/cn/#include \/cn/g' {} \;
+# find ./pages/cn -name index.md -type f -exec sed -i '' -e 's/#include \/cn/#include \/cn/g' {} \;
+find ./pages/cn -name index.md -type f -exec sed -i '' -e 's/](\/1.10/](\/cn\/1.11/g' {} \;
+find ./pages/cn -name index.md -type f -exec sed -i '' -e 's/](\/1.9/](\/cn\/1.11/g' {} \;
+find ./pages/cn -name index.md -type f -exec sed -i '' -e 's/](\/1.8/](\/cn\/1.11/g' {} \;
 
 # bad code blocks
 
