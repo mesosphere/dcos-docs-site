d211b10ef959dd5dd535bc3e0f7511bdcdd50f5c
niartenyaw <amwayne90@gmail.com>
2019-01-08 16:22:07 -0800

    Add partial 307 work

--- redirects/lib/redirect_map.rb
+++ redirects/lib/redirect_map.rb
@@ -3,10 +3,12 @@ require_relative 'redirect_string'
 require_relative 'redirect_regex'
 
 class RedirectMap
-  def initialize(redirect_filename:, existing_filenames:)
-    @redirect_filename = redirect_filename
-    @redirects = populate_map(redirect_filename)
+  def initialize(filename_301:, filename_307:, existing_filenames:)
+    @redirect_filenames = redirect_filenames
     @existing_filenames = existing_filenames
+
+    populate_maps(filename_301: filename_301,
+                  filename_307: filename_307)
   end
 
   def find(data:)
@@ -34,14 +36,39 @@ class RedirectMap
 
   private
 
-  attr_reader :redirects, :existing_filenames
+  attr_reader :redirect_filenames,
+              :redirects_301,
+              :redirects_307,
+              :existing_filenames
 
   def file_exists_for?(link:)
     existing_filenames.file_exists_for?(link: link)
   end
 
   def find_redirect(link:)
-    redirects.each do |redirect|
+    # This is much more complicated than just replacing.
+    # If the link is latest, it should be updated but still
+    # contain latest. A little more involved than a simple
+    # replace
+    # corrected_link = replace_307_redirect(link: link)
+
+    find_301_redirect(link: link)
+  end
+
+  # Returns corrected link if 307 found, else returns original
+  def replace_307_redirect(link:)
+    redirects_307.each do |redirect|
+      if redirect.match?(link: link)
+        return redirect.replace(content: link)
+      end
+    end
+    
+    link
+  end
+
+  # Returns redirect if found, else returns nil
+  def find_301_redirect(link:)
+    redirects_301.each do |redirect|
       if redirect.match?(link: link)
         return redirect
       end
@@ -50,8 +77,15 @@ class RedirectMap
     nil
   end
 
-  def populate_map(filename)
-    File.readlines(filename).map do |line|
+  def populate_maps(filename_301:, filename_307:)
+    populate_map(name: "redirects_301",
+                 filename: filename_301)
+    populate_map(name: "redirects_307",
+                 filename: filename_307)
+  end
+
+  def populate_map(name:, filename:)
+    redirects = File.readlines(filename).map do |line|
       pre_link, post_link = line.split(' ')
 
       # Remove the semicolon
@@ -69,5 +103,6 @@ class RedirectMap
         )
       end
     end
+    instance_variable_set("@#{name}", redirects)
   end
 end
