1a2834eea84cc8a5a93642cf2651ff72f78f7c57
Christina Kiesz <ckiesz@mesosphere.com>
2019-10-24 14:04:58 -0600

    Set latest to 2.0

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ MS.metadata({
     copyright: `&copy; ${currentYear} D2iQ, Inc. All rights reserved.`,
     env: process.env.NODE_ENV,
     gitBranch: GIT_BRANCH,
-    dcosDocsLatest: '1.13',
+    dcosDocsLatest: '2.0',
     dcosCNDocsLatest: '1.12',
     konvoyDocsLatest: 'latest',
     kommanderDocsLatest: '0.1',
