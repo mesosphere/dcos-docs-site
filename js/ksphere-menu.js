cb2889072eaddfe1be75abb3ebf2254af8c4f674
Christina Kiesz <ckiesz@mesosphere.com>
2019-11-15 09:31:07 -0800

    Site code changes

--- js/ksphere-menu.js
+++ js/ksphere-menu.js
@@ -55,5 +55,7 @@ class Spherer {
 
 document.addEventListener('DOMContentLoaded', () => {
     const sphereNav = document.querySelector('#ks');
-    new Spherer(sphereNav);
+    if (sphereNav) {
+      new Spherer(sphereNav);
+    }
 });
--- pages/ksphere/dispatch/latest/index.md
+++ pages/ksphere/dispatch/latest/index.md
@@ -2,7 +2,7 @@
 layout: layout.pug
 navigationTitle: Welcome to Dispatch
 title: Welcome to Dispatch
-enterprise: false
+beta: true
 category: K-Sphere
 menuWeight: 0
 ---
