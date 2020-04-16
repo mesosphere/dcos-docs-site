386e33d319ab923ab58d0750197d6b211d6b9293
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2020-04-16 16:51:42 -0700

    konvoy: use "-beta" suffix
    Signed-off-by: Dimitri Koshkin <dimitri.koshkin@gmail.com>

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ MS.metadata({
     gitBranch: GIT_BRANCH,
     dcosDocsLatest: '2.0',
     dcosCNDocsLatest: '2.0',
-    konvoyDocsLatest: '1.5',
+    konvoyDocsLatest: '1.5-beta',
     kommanderDocsLatest: 'latest',
 });
 
