3d04a75659e7e12111cb09a70866624287c2cafe
msdolbey <dolbes@gmail.com>
2020-06-24 15:57:56 -0500

    makes markdown links that are in a different domain link out to a new tab automatically

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 if (typeof feather !== "undefined") {
   feather.replace();
 }
+
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+   if (links[i].hostname != window.location.hostname) {
+       links[i].target = '_blank';
+   }
+}
