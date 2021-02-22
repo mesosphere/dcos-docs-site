7e2a8c2b779718c489a77b2e4e676a76062eb86f
Pierre Beitz <pierrebeitz@gmail.com>
2021-02-22 21:07:54 +0100

    remove pngs and jpgs that are not referenced

--- scripts/tmp_rm_images.sh
+++ scripts/tmp_rm_images.sh
@@ -1,7 +1,5 @@
-gifs=$(fd .gif)
-
-for f in $gifs; do
-  if ! rg $(basename $f) > /dev/null 2>&1; then
+for f in $(fd .$1); do
+  if ! rg $(basename "$f") > /dev/null 2>&1; then
     echo $f
   fi
 done
