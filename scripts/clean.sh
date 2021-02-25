10408a08b6395268737d41c4ee99ef5303f7368f
Pierre Beitz <pierrebeitz@gmail.com>
2021-02-25 09:29:44 +0100

    remove all scripts
    most scripts are to update services docs. it seems unlikely we ever use
    those again.
    
    `group_cves.js` was for testing the cve-table
    `scripts/clean.sh` has been inlined to the makefile
    `scripts/generate-marathon-api.sh` seemingly has only ever been used
    once. also unlikely that we ever use it again.

--- scripts/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Usage:       clean.sh
-#
-# Description: Removes all build folders generated in the project
-#
-
-rm -rf "./build"
-rm -f ".revision"
--- scripts/service-update-scripts/beta-elastic-merge-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-version=$1
-if [ -z "$version" ]; then echo "Enter a version string for this release as the first argument."; exit 1; fi
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$SCRIPT_DIR/dcos-commons-merge.sh beta-elastic $version "Beta Elastic"
