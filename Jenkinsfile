eb554c0970c8aad0946ed2d6239bbe5141b071d8
Pierre Beitz <pierrebeitz@gmail.com>
2021-03-08 20:24:35 +0100

    WIP

--- Jenkinsfile
+++ Jenkinsfile
@@ -13,10 +13,28 @@ pipeline {
   // triggers {
   //   cron("* * * * *")
   // }
+
   stages {
     stage("Sync") {
       steps {
-        sh './ci/pull-upstreams.sh'
+        withCredentials([string(credentialsId: "d146870f-03b0-4f6a-ab70-1d09757a51fc", variable: "GITHUB_TOKEN")]) {
+          sshagent(credentials : ['4ff09dce-407b-41d3-847a-9e6609dd91b8']) {
+            sh '''#!/bin/bash
+              set -euxo pipefail
+
+              git config user.name "Docs Bot"
+              git config user.email "docs-bot@d2iq.com"
+              git remote set-url origin git@github.com:mesosphere/dcos-docs-site.git
+
+              apk add curl curl-dev rsync --no-cache
+
+              ./ci/pull-upstream.sh mesosphere/konvoy docs/site release-1.7 pages/dkp/konvoy/1.7
+              # ./ci/pull-upstream.sh mesosphere/konvoy docs/site release-1.6 pages/dkp/konvoy/1.6
+              # ./ci/pull-upstream.sh mesosphere/konvoy docs/site release-1.5 pages/dkp/konvoy/1.5
+              # ./ci/pull-upstream.sh mesosphere/konvoy docs/site release-1.4 pages/dkp/konvoy/1.4
+            '''
+          }
+        }
       }
     }
   }
--- ci/pull-upstream.sh
+++ ci/pull-upstream.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Requires git >= 2.19 for sparse checkout
-# Requires gh >= 1.6.2 for pr creation
 # Requires rsync
 
-set -euo pipefail
+set -euxo pipefail
 IFS=$'\n\t'
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../ >/dev/null 2>&1 && pwd)"
 
@@ -14,13 +13,18 @@ REPO_BRANCH="$3"                         # e.g. release-1.7
 DOCS_SUBFOLDER="$4"                      # e.g. pages/dkp/konvoy/1.7
 REPO_URL="git@github.com:$REPO_NAME.git"
 
+git checkout -t origin/main || git checkout main || echo "on main"
+
 echo "Cloning $REPO_URL on branch $REPO_BRANCH"
 
 TMP_DIR="$PROJECT_ROOT/sync"
 mkdir -p "$TMP_DIR"
 cd "$PROJECT_ROOT/sync"
-[ ! -d "$REPO_NAME" ] && git clone "https://github.com/$REPO_NAME" $REPO_NAME
+
+[ ! -d "$REPO_NAME" ] && git clone "$REPO_URL" "$REPO_NAME"
+
 cd "$PROJECT_ROOT/sync/$REPO_NAME"
+# this helps if you don't remove the repo locally.
 git fetch
 git checkout "$REPO_BRANCH"
 SYNC_REPO_REF="$(git rev-parse --short HEAD)"
@@ -32,15 +36,16 @@ cd "$PROJECT_ROOT/$DOCS_SUBFOLDER/"
 if [ -z "$(git status --porcelain)" ]; then
     echo "No changes."
 else
-    BRANCH="docs-sync-$REPO_BRANCH-$SYNC_REPO_REF"
+    BRANCH="autosync-$REPO_BRANCH-$SYNC_REPO_REF"
     echo "Creating PR against docs with branch $BRANCH"
     git checkout -b "$BRANCH" || git checkout "$BRANCH"
     git add --all
     git commit -m  "docs: sync with $REPO_NAME:$REPO_BRANCH"
+    git push origin "$BRANCH" -uf
 
-    printf '\n' | gh pr create \
-      --title "Docs: Sync $REPO_NAME:$REPO_BRANCH with docs repo" \
-      --body "This is an automated PR, no humans were harmed creating this PR. You can find the script in the docs repo." \
-      --base main
-    git checkout main
+    curl \
+      -X POST \
+      -H "Accept: application/vnd.github.v3+json" \
+      https://$GITHUB_TOKEN@api.github.com/repos/mesosphere/dcos-docs-site/pulls \
+      -d '{"head":"'$BRANCH'","base":"main", "title": "Sync '$REPO_NAME:$REPO_BRANCH'"}'
 fi
