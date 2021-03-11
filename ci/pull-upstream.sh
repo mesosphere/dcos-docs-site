42649c91b80fbc411f3fd97bbb328fc8eff8c774
Pierre Beitz <pierrebeitz@gmail.com>
2021-03-11 18:21:35 +0100

    update sync-scripts

--- ci/pull-upstream.sh
+++ ci/pull-upstream.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Requires git >= 2.19 for sparse checkout
-# Requires gh >= 1.6.2 for pr creation
 # Requires rsync
 
-set -euo pipefail
+set -euxo pipefail
 IFS=$'\n\t'
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../ >/dev/null 2>&1 && pwd)"
 
@@ -14,33 +13,39 @@ REPO_BRANCH="$3"                         # e.g. release-1.7
 DOCS_SUBFOLDER="$4"                      # e.g. pages/dkp/konvoy/1.7
 REPO_URL="git@github.com:$REPO_NAME.git"
 
+git checkout -t origin/main || git checkout main || echo "on main"
+
 echo "Cloning $REPO_URL on branch $REPO_BRANCH"
-TMP_DIR="$(cd "$PROJECT_ROOT/$(mktemp -d sync.XXXXX)" && pwd)"
-cd "$TMP_DIR"
-gh repo clone "$REPO_NAME" upstream -- -b "$REPO_BRANCH"
-cd upstream
+
+TMP_DIR="$PROJECT_ROOT/sync"
+mkdir -p "$TMP_DIR"
+cd "$PROJECT_ROOT/sync"
+
+[ ! -d "$REPO_NAME" ] && git clone "$REPO_URL" "$REPO_NAME"
+
+cd "$PROJECT_ROOT/sync/$REPO_NAME"
+# this helps if you don't remove the repo locally.
+git fetch
+git checkout "$REPO_BRANCH"
 SYNC_REPO_REF="$(git rev-parse --short HEAD)"
 
 echo "Diffing repo $REPO_SUBFOLDER against docs $DOCS_SUBFOLDER"
-rsync -a "$TMP_DIR/upstream/$REPO_SUBFOLDER/" "$PROJECT_ROOT/$DOCS_SUBFOLDER/"
+rsync -a "$TMP_DIR/$REPO_NAME/$REPO_SUBFOLDER/" "$PROJECT_ROOT/$DOCS_SUBFOLDER/"
 cd "$PROJECT_ROOT/$DOCS_SUBFOLDER/"
 
-STATUS=$(git status)
 if [ -z "$(git status --porcelain)" ]; then
-    echo "No changes found: $STATUS"
+    echo "No changes."
 else
-    echo "Changes found $STATUS"
-    BRANCH="docs-sync-$REPO_BRANCH-$SYNC_REPO_REF"
-
+    BRANCH="autosync-$REPO_BRANCH-$SYNC_REPO_REF"
     echo "Creating PR against docs with branch $BRANCH"
+    git checkout -b "$BRANCH" || git checkout "$BRANCH"
     git add --all
-    git checkout -b "$BRANCH"
     git commit -m  "docs: sync with $REPO_NAME:$REPO_BRANCH"
+    git push origin "$BRANCH" -uf
 
-    printf '\n' | gh pr create \
-      --title "Docs: Sync $REPO_NAME:$REPO_BRANCH with docs repo" \
-      --body "This is an automated PR, no humans were harmed creating this PR. You can find the script in the docs repo." \
-      --base main
+    curl \
+      -X POST \
+      -H "Accept: application/vnd.github.v3+json" \
+      https://$GITHUB_TOKEN@api.github.com/repos/mesosphere/dcos-docs-site/pulls \
+      -d '{"head":"'$BRANCH'","base":"main", "title": "Sync '$REPO_NAME:$REPO_BRANCH'"}'
 fi
-
-rm -rf "$TMP_DIR"
