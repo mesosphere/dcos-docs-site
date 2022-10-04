7e6856c205ea9c4a705e77224db718654907c200
Hrant Mirijanyan <hrant.mirijanyan@gmail.com>
2022-10-04 15:48:15 -0500

    pass var to script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -13,7 +13,7 @@ envsubst < s3config.json > .s3config.json
 # prepare and set up bucket. if it's present we'll just do it all over for now.
 echo "PRINTING STUFF"
 echo $BUCKET
-echo ${{ env.BUCKET }}
+echo $1
 echo $AWS_DEFAULT_REGION
 aws s3api create-bucket --bucket $BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
 
