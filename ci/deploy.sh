4cdcaac61f3ccc128c06f9d0cd1e5b6c18e6be60
Hrant Mirijanyan <hrant.mirijanyan@gmail.com>
2022-10-04 15:59:08 -0500

    add additional log

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -15,6 +15,7 @@ envsubst < s3config.json > .s3config.json
 aws s3api create-bucket --bucket $BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
 
 if [ "$BUCKET" != "docs-d2iq-com-preview" ]; then
+  echo 'INSIDE BLOCK'
   aws s3api put-public-access-block --bucket $BUCKET --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
   aws s3api put-bucket-policy --bucket $BUCKET --policy file:///src/.policy
   aws s3api put-bucket-website --bucket $BUCKET --website-configuration file:///src/.s3config.json
