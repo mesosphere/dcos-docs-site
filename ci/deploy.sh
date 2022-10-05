c3554c2df1f2f870a3e31c499a2ac75ac33750fd
Hrant Mirijanyan <hrant.mirijanyan@gmail.com>
2022-10-05 17:15:18 -0500

    remove logging

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -15,11 +15,9 @@ envsubst < s3config.json > .s3config.json
 aws s3api create-bucket --bucket $BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
 
 if [ "$BUCKET" != "docs-d2iq-com-preview" ]; then
-  echo 'INSIDE BLOCK'
-  echo $BUCKET
   aws s3api put-public-access-block --bucket $BUCKET --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
   aws s3api put-bucket-policy --bucket $BUCKET --policy file:///src/.policy
   aws s3api put-bucket-website --bucket $BUCKET --website-configuration file:///src/.s3config.json
 fi
-echo 'Delete step'
+
 aws s3 sync --delete --quiet --acl bucket-owner-full-control ./build s3://$BUCKET
