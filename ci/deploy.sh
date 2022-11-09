9dfbbb00a57e06a747bff4b0b95b6a5cdb774aa5
Hrant Mirijanyan <hrant.mirijanyan@gmail.com>
2022-11-09 16:19:19 +0300

    add back commented out code

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -12,7 +12,7 @@ envsubst < s3bucketpolicy > .policy
 envsubst < s3config.json > .s3config.json
 
 # prepare and set up bucket. if it's present we'll just do it all over for now.
-# aws s3api create-bucket --bucket $BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
+aws s3api create-bucket --bucket $BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
 
 if [ "$BUCKET" != "docs-d2iq-com-preview" ]; then
   aws s3api put-public-access-block --bucket $BUCKET --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
@@ -20,4 +20,4 @@ if [ "$BUCKET" != "docs-d2iq-com-preview" ]; then
   aws s3api put-bucket-website --bucket $BUCKET --website-configuration file:///src/.s3config.json
 fi
 
-# aws s3 sync --delete --quiet --acl bucket-owner-full-control ./build s3://$BUCKET
+aws s3 sync --delete --quiet --acl bucket-owner-full-control ./build s3://$BUCKET
